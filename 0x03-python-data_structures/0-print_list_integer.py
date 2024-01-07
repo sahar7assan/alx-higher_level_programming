8f16cf9cc4a7d761ca1811a14dae34978060f043
sahar7assan <sahar.h.ahmed@gmail.com>
2024-01-07 09:48:58 -0800

    print list element

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Function print list element
+
+    Args:
+        my_list:list of element
+
+    Returns:
+        nothing
+    """
+    for i in my_list:
+        print("{}".format(i))
