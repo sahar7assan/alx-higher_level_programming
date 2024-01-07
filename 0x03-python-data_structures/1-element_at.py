d13ea5ad97ad570166ae158103b439d295016929
sahar7assan <sahar.h.ahmed@gmail.com>
2024-01-07 10:15:25 -0800

    retrive elment at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """retrieves an elemen
+
+    Args:
+        my_list:list element
+        idx:element index
+
+    Returns:
+        element in index idx
+    """
+    length = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > length:
+        return None
+    else:
+        return my_list[idx]
