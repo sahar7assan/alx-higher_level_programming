0ecd43da43633cac824ce7530652ed472241d03a
sahar7assan <sahar.h.ahmed@gmail.com>
2024-01-08 13:06:20 -0800

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Function return length and first char
+
+    Args:
+        sentence: string
+
+    Returns:
+        tuple
+    """
+    length = len(sentence)
+    if length > 0:
+        first = senctence[0]
+
+    else:
+        first = None
+    return (length, first)
