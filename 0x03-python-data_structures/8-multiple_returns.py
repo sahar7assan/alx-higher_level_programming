c8dde5c82fe224a854f1710ed44c5264483ab7a4
sahar7assan <sahar.h.ahmed@gmail.com>
2024-01-08 13:11:56 -0800

    multipe return edit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -10,7 +10,7 @@ def multiple_returns(sentence):
     """
     length = len(sentence)
     if length > 0:
-        first = senctence[0]
+        first = sentence[0]
 
     else:
         first = None
