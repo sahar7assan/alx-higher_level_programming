59af352b781d77cd94a6ef46833e447ae47f9402
sahar7assan <sahar.h.ahmed@gmail.com>
2024-01-10 12:15:12 -0800

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """function update dictionary
+
+    Args:
+        a_dictionary: dictionary
+        key: key
+        value: value to update
+
+    Returns:
+        updated dictionary
+    """
+    a_dictionary[key] = value
+    return (a_dictionary)
